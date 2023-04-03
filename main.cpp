bac4436dab2045d31d65c71f66d29b79df84ca98
avprod75 <andrio2019voyt@gmail.com>
2023-04-03 10:22:13 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x, y, z;
+  cout << "culculate" << endl;
+  cout << " Enter number :";
+  cin >> x;
+  cout << " Enter number:";
+  cin >> y;
+  cout << " Enter number:";
+  cin >> z;
+  cout << "Here  is the sum:" << x + y + z << endl;
+  cout << "=" << x * y * z << endl;
+  return 0;
+}
